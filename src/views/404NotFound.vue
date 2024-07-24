735a014b2c06f03b9a051a5fd1be55d74c9c4a08
Giovani Zanetti <zanetti.giovani@gmail.com>
2024-07-24 10:44:05 +0200

    create 404 page (#7)
    Co-authored-by: Giovani Zanetti <g.zanetti@picturae.com>

--- src/views/404NotFound.vue
+++ src/views/404NotFound.vue
@@ -1,4 +1,36 @@
+<script lang="ts" setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+</script>
+
 <template>
-  <div>404</div>
+  <main class="main">
+    <h1>404</h1>
+    <span>Not Found</span>
+    <span @click="router.back()" class="back">Go back</span>
+  </main>
 </template>
-<script lang="ts" setup></script>
+
+<style scoped lang="scss">
+.main {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  background-color: $black-1;
+  height: 100vh;
+  color: $white-1;
+  font-weight: 700;
+  font-size: $typography-5;
+
+  span {
+    font-size: $typography-3;
+  }
+  .back {
+    font-size: $typography-1;
+    cursor: pointer;
+    text-decoration: underline;
+  }
+}
+</style>
