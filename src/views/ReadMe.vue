4240d4b23c10728891c17ffdf6a86f668ac0dff0
Giovani Zanetti <g.zanetti@picturae.com>
2024-07-29 13:06:43 +0200

    fix style readme page

--- src/views/ReadMe.vue
+++ src/views/ReadMe.vue
@@ -19,29 +19,34 @@
       improve this assignment. I'm very excited by the possibility of working
       for Constell, I don't want to miss this opportunity.
     </p>
-    <h3 class="section-heading">
-      Some extra features on top of the requirements 👇🏻
-    </h3>
-    <ul>
-      <li>Translations <i>(English and Dutch)</i></li>
-      <li>Feedback with toast notification</li>
-      <li>Tooltips</li>
-      <li>Transitions</li>
-      <li>Drawer side navigation</li>
-      <li>404 page</li>
-    </ul>
 
-    <h3 class="section-heading">What If I had to build more features 👇🏻</h3>
-    <ul>
-      <li>I would add the possibility of performing CRUD operations on team</li>
-      <li>I would add pagination for the user's list.</li>
-      <li>I would add a light /dark theme toggle.</li>
-      <li>I would make a photo upload functionality</li>
-      <li>I would look for opportunities to improve the UX</li>
-      <li>More tests</li>
+    <section>
+      <h3 class="section-heading">
+        Some extra features on top of the requirements 👇🏻
+      </h3>
+      <ul>
+        <li>Translations <i>(English and Dutch)</i></li>
+        <li>Feedback with toast notification</li>
+        <li>Tooltips</li>
+        <li>Transitions</li>
+        <li>Drawer side navigation</li>
+        <li>404 page</li>
+      </ul>
+    </section>
 
-    </ul>
-
+    <section>
+      <h3 class="section-heading">What If I had to build more features 👇🏻</h3>
+      <ul>
+        <li>
+          I would add the possibility of performing CRUD operations on team
+        </li>
+        <li>I would add pagination for the user's list.</li>
+        <li>I would add a light /dark theme toggle.</li>
+        <li>I would make a photo upload functionality</li>
+        <li>I would look for opportunities to improve the UX</li>
+        <li>More tests</li>
+      </ul>
+    </section>
     <a
       class="git-hub-link"
       href="https://github.com/giovanizanetti/Venue-people#readme"
@@ -52,7 +57,12 @@
 
 <style lang="scss" scoped>
 .readme {
-  padding: 7rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  @media screen and (min-width: $tablet-md) {
+    padding: 7rem;
+  }
   .heading {
     font-size: $typography-5;
     text-align: center;
@@ -60,27 +70,33 @@
   .sub-heading {
     font-size: $typography-4;
     padding: $margin-lg $margin-xl;
+
   }
   .section-heading {
+    text-align: center;
     font-size: $typography-3;
+    font-weight: 600;
   }
-  ul {
-    margin: $margin-md $margin-xl;
-    li {
-      list-style: disc;
-      font-size: 1.2rem;
+  section {
+    ul {
+      text-align: center;
+      margin: $margin-md $margin-xl;
+      li {
+        text-align: center;
+        font-size: 1.2rem;
+      }
     }
   }
 
   p.text {
     margin: $margin-md;
-    font-size: 20px
+    font-size: 20px;
   }
 
   .git-hub-link {
     display: flex;
     justify-content: center;
-    font-size: 1.5rem;
+    font-size: 1.8rem;
     text-decoration: underline;
     font-weight: 600;
     margin-top: $margin-lg;
